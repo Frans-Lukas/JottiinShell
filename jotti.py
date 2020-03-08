80dd8ce0ed95523ca0394470a417230bcafd430e
Frans-Lukas <franslukas@gmail.com>
2020-03-08 13:11:26 +0100

    posting and getting notes works!

--- jotti.py
+++ jotti.py
@@ -1,8 +1,13 @@
 import argparse
 import configparser
+import json
 import os
 import getpass
 import sys
+from datetime import datetime
+from dateutil import parser
+
+import requests
 
 
 def ask_for_password():
@@ -20,6 +25,7 @@ def change_or_create_credential_store():
     config[CREDENTIALS_KEY][PASSWORD_KEY] = ask_for_password()
     with open(CREDENTIAL_STORE_PATH, 'w') as configfile:
         config.write(configfile)
+    print('Credentials stored in ' + CREDENTIAL_STORE_PATH)
 
 
 def ask_for_username():
@@ -59,26 +65,52 @@ def authenticate():
 PASSWORD_KEY = 'password'
 USERNAME_KEY = 'username'
 CREDENTIALS_KEY = 'credentials'
-
-NO_JOTT = 'no_jott'
-
+API = 'https://jotti.in/api'
+TEST_API = 'http://localhost:8000/api'
+AP_JOTTS = '/jotts/'
+EMPTY_ARGUMENT = 'no_jott'
 CREDENTIAL_STORE_PATH = 'jotti_credential_store.ini'
 
-parser = argparse.ArgumentParser(description='Take notes')
-g = parser.add_mutually_exclusive_group()
-g.add_argument('--jott', '--take-note', type=str, help='Stores the given note.', default=NO_JOTT)
+argparser = argparse.ArgumentParser(description='Take notes')
+g = argparser.add_mutually_exclusive_group()
+g.add_argument('--jott', '--take-note', type=str, help='Stores the given note.', default=EMPTY_ARGUMENT)
 g.add_argument('--auth', '--login', dest='auth', action='store_true', default=False,
                help='stores jotti.in credentials for future use.')
 g.add_argument('--notes', '--view-notes', '--jotts', dest='view_notes', action='store_true', default=False,
                help='Shows the latest notes.')
 
-args = parser.parse_args()
+args = argparser.parse_args()
 
 if args.auth:
     change_or_create_credential_store()
 
-if not args.jott == NO_JOTT:
+
+def post_note(username, password, text):
+    response = requests.post(TEST_API + AP_JOTTS, data={'note_text': text}, auth=(username, password))
+    print(response.text)
+
+
+if not args.jott == EMPTY_ARGUMENT:
     username, password = authenticate()
+    post_note(username, password, args.jott)
+
+
+def get_jotts(username, password):
+    next_url = TEST_API + AP_JOTTS
+    while next_url is not None:
+        response = requests.get(next_url, auth=(username, password))
+        parsed_json = response.json()
+        if 'detail' in parsed_json:
+            print(parsed_json['detail'])
+            break
+        else:
+            next_url = parsed_json['next']
+            results = parsed_json['results']
+            for result in results:
+                date = parser.parse(result['pub_date']).strftime('%Y-%m-%d %H:%M:%S')
+                print(str(date) + ": " + result['note_text'])
+
 
 if args.view_notes:
-    print('view notes')
+    username, password = authenticate()
+    get_jotts(username, password)
